d82e500705173e857d5916f239efb77af1c5b01c
James Halliday <substack@gmail.com>
2010-12-14 07:35:30 +0000

    basic proxy test passes

--- test/proxy.js
+++ test/proxy.js
@@ -2,10 +2,6 @@ var socks5 = require('prox').socks5;
 
 exports.socks5 = function (assert) {
     var port = Math.floor(10000 + Math.random() * (Math.pow(2,16) - 10000));
-    var to = setTimeout(function () {
-        assert.fail('Never got request');
-    }, 500);
-    
     var tc = setTimeout(function () {
         assert.fail('Never connected');
     }, 500);
@@ -13,12 +9,18 @@ exports.socks5 = function (assert) {
     var td = setTimeout(function () {
         assert.fail('Never got data');
     }, 500);
+    
+    var to = setTimeout(function () {
+        assert.fail('Never ended');
+    }, 500);
      
-    socks5.createServer(function (err, req, stream) {
+    var server = socks5.createServer(function (err, req, stream) {
         assert.eql(req.host, 'moo');
         assert.eql(req.port, 8080);
         stream.write(new Buffer('oh hello'));
-    }).listen(port, ready);
+        stream.end();
+    });
+    server.listen(port, ready);
     
     function ready () {
         var stream = socks5
@@ -33,10 +35,12 @@ exports.socks5 = function (assert) {
         stream.on('data', function (buf) {
             clearTimeout(td);
             assert.eql(buf.toString(), 'oh hello');
+            stream.end();
         });
         
         stream.on('end', function () {
             clearTimeout(to);
+            server.close();
         });
     }
 };
